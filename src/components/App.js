590ffb78208cbabbc4d8265de3d981977d8de044
Guilherme Duarte <gui13dula@gmail.com>
2018-06-18 15:53:24 -0300

    Bottom info fix

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ import code from '../assets/images/projects/code.png'
 import splay from '../assets/images/projects/splay.png'
 import uplab from '../assets/images/projects/uplab.png'
 
+import siteCursor from '../assets/images/cursor.svg'
+
 class App extends Component {
   constructor() {
     super();
@@ -47,10 +49,11 @@ class App extends Component {
   }
   render() {
 
-    var projectQuantity = 0
-var totalWidth = 0
-var projectWidth = 0
-var offset = 0
+  var projectQuantity = 0
+  var totalWidth = 0
+  var projectWidth = 0
+  var offset = 0
+  var cursor = siteCursor
     
 window.addEventListener('load', ()=>{
   projectWidth = document.querySelectorAll('.project')[0].clientWidth + 40
@@ -63,6 +66,7 @@ window.addEventListener('load', ()=>{
   let traco
   let space
   let bottom
+  let grayScaleThumb
   let timeLine1
 
   document.querySelector('.app').addEventListener('mousedown', (e) => {
@@ -70,39 +74,42 @@ window.addEventListener('load', ()=>{
 
       t1 = TweenMax.to(".project", 0.5, {"min-width":"100%", width: "100%"})
       space = TweenMax.to(".others", 0.5, {transform: "translateX(-10%)"})
-      t2 = TweenMax.to(".thumb-mask", 0.3, {width: "70%", filter: "grayscale(100%) brightness(1.2)"})
+      t2 = TweenMax.to(".thumb-mask", 0.85, {width: "70%"})
+      grayScaleThumb = TweenMax.to(".thumb-mask", 0.3, {filter: "grayscale(100%) brightness(1.2)"})
       t3 = TweenMax.to(".thumb", 0.5, {opacity: 0.3})
       t4 = TweenMax.to(".identity", 0.5, {opacity: 1})
-      t5 = TweenMax.to(".identity h1", 0.8, {transform:"translateX(0)", delay: 0.1})
-      t6 = TweenMax.to(".identity h2", 0.8, {transform:"translateY(0)", delay: 0.1})
+      t5 = TweenMax.to(".identity h1", 0.8, {transform:"translateX(0)", delay: 0.2})
+      t6 = TweenMax.to(".identity h2", 0.8, {transform:"translateY(0)", delay: 0.2})
       traco = TweenMax.to(".traco", 1.5, {transform:"scale(1,1)"})
       bottom = TweenMax.to(".bottom", 0.3, {opacity: 0})
+      document.documentElement.style.cursor = 'url('+cursor+') 64 64, auto';
       
       timeLine1 = new TimelineMax()
   })
 
   document.querySelector('.app').addEventListener('mouseup', (e) => {
       document.querySelector('.app').removeEventListener("mousemove", horizontalNavigation)
-      if(projectWidth % offset != 0){
           let last
           let maior
           let projects = document.querySelectorAll('.project')
           for (let i = 0 ; i < projects.length ; i++) {
+              this.setState({active: i})
               if((projects[i].offsetLeft + 10) * -1 < offset) {
                   offset = projects[i].offsetLeft * -1
                   this.setState({active: i})
+                  console.log(i)
                   i = projects.length + 1
               }
           }
           document.querySelector('#projects').style.transform = "translateX("+offset+"px)"
-      }
       t1.reverse()
-      t2 = TweenMax.to(".thumb-mask", 0.3, {width: "100%", filter: "grayscale(0%) brightness(1)"})
+      t2 = TweenMax.to(".thumb-mask", 0.85, {width: "100%"})
       t3.reverse()
       t4.reverse()
       t5.reverse()
       t6.reverse()
       space.reverse()
+      grayScaleThumb.reverse()
       traco = TweenMax.to(".traco", 1.5, {transform:"scale(0,1)"})
       bottom.reverse()
   })
